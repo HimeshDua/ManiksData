f1ac65108522225e922ead036501eb0f54737c47
HimeshPremji <himeshdmaheswary@gmail.com>
2025-01-28 03:15:53 +0500

    images added

---
+++ graphics/graphics.js
@@ -0,0 +1,31 @@
+import banner from "../graphics/banner.webp";
+import bigE1 from "../graphics/bigE1.webp";
+import bigE2 from "../graphics/bigE2.webp";
+import e1 from "../graphics/e1.webp";
+import e2 from "../graphics/e2.webp";
+import e3 from "../graphics/e3.webp";
+import e4 from "../graphics/e4.jpg";
+import e5 from "../graphics/e5.jpg";
+import e6 from "../graphics/e6.webp";
+import e7 from "../graphics/e7.webp";
+import e8 from "../graphics/e8.jpg";
+import e9 from "../graphics/e9.webp";
+import icon from "../graphics/icon.png";
+import webDevIcon from "../graphics/web-development-icon.svg";
+
+export const graphics = {
+  banner,
+  bigE1,
+  bigE2,
+  e1,
+  e2,
+  e3,
+  e4,
+  e5,
+  e6,
+  e7,
+  e8,
+  e9,
+  icon,
+  webDevIcon,
+};
